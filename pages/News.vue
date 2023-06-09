339d9325f4844286e5192f8ae104f4b9a4e94e15
codeking254 <codecenter000@gmail.com>
2023-06-09 23:35:02 +0300

    post-5

--- pages/News.vue
+++ pages/News.vue
@@ -3,7 +3,7 @@
     <div class="max-w-7xl text-center mx-auto px-4 sm:px-6 lg:px-8 py-16">
       <h1 class="text-3xl justify-text-center font-extrabold bg-gradient-to-r from-accent to-secondary text-gray-900 inline-block rounded-full px-6 py-2">NEWS</h1>
       <div class="mt-12 grid gap-5 md:grid-cols-2 lg:grid-cols-3">
-        <div v-for="item in posts" :key="item.id" class=" backdrop-blur-3xl  shadow-lg rounded-lg overflow-hidden">
+        <div v-for="item in posts" :key="item.id" class="backdrop-blur-3xl shadow-lg rounded-lg overflow-hidden">
           <!-- Display news item image -->
           <div class="relative">
             <img class="h-48 w-full object-cover rounded-t-lg" :src="item.image" :alt="item.title" />
@@ -45,57 +45,56 @@
 
 <script>
 import { onMounted, ref, nextTick } from 'vue';
-import post1 from '@/static/images/news/post-5.jpg';
-import post2 from '@/static/images/news/post-6.jpg';
-import post3 from '@/static/images/news/post-6.jpg';
-import post4 from '@/static/images/news/post-5.jpg';
-import post6 from '@/static/images/news/post-5.jpg';
-import post5 from '@/static/images/news/post-6.jpg';
+import post1 from '@/static/images/news/post-1.jpg';
+import post2 from '@/static/images/news/post-2.jpg';
+import post3 from '@/static/images/news/post-3.jpg';
+import post4 from '@/static/images/news/post-4.jpg';
+import post5 from '@/static/images/news/post-5.jpg';
+import post6 from '@/static/images/news/post-6.jpg';
 
 definePageMeta({ auth: false });
 export default {
   props: {
-   
     href: String,
   },
-   setup() {
-    const posts =[
+  setup() {
+    const posts = [
       {
         title: 'Schield Center Celebrates 10 Years of Excellence in Education',
         image: post1,
         description: 'Schield Center has been providing quality education to students in Kajiado for the past decade. To celebrate this milestone, the school hosted a series of events...',
-        link: '#'
+        link: '#',
       },
       {
         title: 'Schield Center Receives Award for Best Performing School in Kajiado',
         image: post2,
         description: 'Schield Center was recently recognized as the best performing school in Kajiado County. The award was presented by the County Governor...',
-        link: '#'
+        link: '#',
       },
       {
         title: 'Schield Center Launches New STEM Program for Students',
         image: post3,
         description: 'Schield Center is proud to announce the launch of its new STEM program for students. The program will provide students with hands-on...',
-        link: '#'
+        link: '#',
       },
       {
         title: 'Schield Center Students Win National Science Competition',
         image: post4,
         description: 'A team of Schield Center students recently won the National Science Competition. The team developed a new method for...',
-        link: '#'
+        link: '#',
       },
       {
         title: 'Schield Center Partners with Local NGOs to Improve Education in Rural Areas',
         image: post5,
         description: 'Schield Center has partnered with local NGOs to improve access to education in rural areas. The school will be providing resources and training...',
-        link: '#'
+        link: '#',
       },
       {
         title: 'Schield Center Launches New Athletics Program for Students',
         image: post6,
         description: 'Schield Center is excited to announce the launch of its new athletics program for students. The program will offer a variety of sports and activities...',
-        link: '#'
-      }
+        link: '#',
+      },
     ];
     const selectedPost = ref(null);
 
